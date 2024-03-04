9663093086a674dcc8507fbd62180ccb21fba5a2
Phil Weir <phil.weir@flaxandteal.co.uk>
2024-03-04 17:01:12 +0100

    fix(ci): need some templating in recipe

--- .github/workflows/generate_recipe.py
+++ .github/workflows/generate_recipe.py
@@ -1,11 +1,22 @@
-from jinja2 import Environment, FileSystemLoader
+import textwrap
 import toml
 
 with open("pyproject.toml") as f:
     pyproject = toml.load(f)
 
-with open(".github/workflows/recipe.yaml.j2") as f:
+with open("README.md") as f:
+    readme_string = f.read()
+
+readme_string = textwrap.indent(readme_string, "    ")
+
+with open("recipe.yaml") as f:
     TEMPLATE = f.read()
 
-template = Environment(loader=FileSystemLoader("./")).from_string(TEMPLATE)
-print(template.render(pyproject=pyproject))
+deps = pyproject["project"]["dependencies"]
+deps_string = ""
+for dep in deps:
+    deps_string += f"    - {dep}\n"
+TEMPLATE = TEMPLATE.replace("__PROJECT_DEPENDENCIES__", deps_string)
+TEMPLATE = TEMPLATE.replace("__README__", readme_string)
+with open("recipe.yaml", "w") as f:
+    f.write(TEMPLATE)
