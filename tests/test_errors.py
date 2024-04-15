976e3a1d5577c7951c19f398f01cde1e7690f8c6
Phil Weir <phil.weir@flaxandteal.co.uk>
2024-04-15 10:34:10 +0100

    fix(errors): make sure error is still seen even in a nested task

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,12 +1,17 @@
 """Test for expected errors."""
 
 import pytest
-from dewret.tasks import construct, task
+from dewret.tasks import construct, task, nested_task
 
 @task()
 def add_task(left: int, right: int) -> int:
     """Adds two values and returns the result."""
     return left + right
+
+@nested_task()
+def badly_add_task(left: int, right: int) -> int:
+    """Badly attempts to add two numbers."""
+    return add_task(left=left) # type: ignore
 
 def test_missing_arguments_throw_error() -> None:
     """Check whether omitting a required argument will give an error.
@@ -20,7 +25,21 @@ def test_missing_arguments_throw_error() -> None:
     result = add_task(left=3) # type: ignore
     with pytest.raises(TypeError) as exc:
         construct(result)
-        assert str(exc) == "TypeError: missing a required argument: 'right'"
+    assert str(exc.value) == "missing a required argument: 'right'"
+
+def test_missing_arguments_throw_error_in_nested_task() -> None:
+    """Check whether omitting a required argument will give an error.
+
+    Since we do not run the original function, it is up to dewret to check
+    that the signature is, at least, acceptable to Python.
+
+    WARNING: in keeping with Python principles, this does not error if types
+    mismatch, but `mypy` should. You **must** type-check your code to catch these.
+    """
+    result = badly_add_task(left=3, right=4)
+    with pytest.raises(TypeError) as exc:
+        construct(result)
+    assert str(exc.value) == "missing a required argument: 'right'"
 
 def test_positional_arguments_throw_error() -> None:
     """Check whether we can produce simple CWL.
@@ -31,4 +50,4 @@ def test_positional_arguments_throw_error() -> None:
     result = add_task(3, right=4)
     with pytest.raises(TypeError) as exc:
         construct(result)
-        assert str(exc) == "TypeError: missing a required argument: 'right'"
+    assert str(exc.value) == "Calling add_task: Arguments must _always_ be named, e.g. my_task(num=1) not my_task(1)"
