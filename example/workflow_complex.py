833740d1a216268e6a8a0177287e37f37d5abe52
Phil Weir <phil.weir@flaxandteal.co.uk>
2024-04-15 09:38:37 +0100

    fix(example): better function naming

--- example/workflow_complex.py
+++ example/workflow_complex.py
@@ -14,7 +14,7 @@ from extra import sum, double, increase
 STARTING_NUMBER: int = 23
 
 @nested_task()
-def run() -> int | float:
+def nested_workflow() -> int | float:
     """Creates a graph of task calls."""
     left = double(num=increase(num=STARTING_NUMBER))
     right = increase(num=increase(num=17))
