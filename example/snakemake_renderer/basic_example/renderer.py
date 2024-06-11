b5e9615ecccb0023c9766adc24513f2924e85129
KamenDimitrov97 <kamen.dimitrov@flaxandteal.co.uk>
2024-06-11 14:51:52 +0300

    feat(snakemake): Added a snakemake renderer, left todos for feature approval.

--- example/snakemake_renderer/basic_example/renderer.py
+++ example/snakemake_renderer/basic_example/renderer.py
@@ -16,25 +16,36 @@
 
 Outputs a [Snakemake](https://snakemake.github.io/) representation of the
 current workflow.
+
 """
+
 import os
 import yaml
 import inspect
+import typing
 
 from attrs import define
 from dewret.workflow import Lazy
 from dewret.workflow import Reference, Raw, Workflow, Step, Task
-from dewret.utils import RawType
-from collections.abc  import ItemsView
+from dewret.utils import BasicType
 
-# TODO: Write: Better description of classes.
-
-# TODO: Define: Inputschema
+RawType = typing.Union[BasicType, list[str], list["RawType"], dict[str, "RawType"]]
 
 
 @define
 class ReferenceDefinition:
-    """Snakemake-renderable internal reference."""
+    """Represents a Snakemake-renderable internal reference.
+
+    Attributes:
+        source (str): The source of the internal reference.
+
+    Methods:
+        from_reference(cls, ref: Reference) -> "ReferenceDefinition": Constructs a
+            ReferenceDefinition object from a Reference object, extracting the source
+            of the reference.
+        render(self) -> str: Renders the internal reference definition as a string
+            suitable for use in Snakemake workflows.
+    """
 
     source: str
 
@@ -50,11 +61,31 @@ class ReferenceDefinition:
         return cls(source=ref.name)
 
     def render(self) -> str:
+        """Render the internal reference definition as a string.
+
+        Returns:
+            str: internal reference.
+        """
         return self.source
+
 
 # TODO: Refactor: better way to handle the types.
 def to_snakemake_type(param: Raw) -> str:
-    """Snakemake-renderable internal reference."""
+    """Convert a raw type to a corresponding Snakemake-compatible Python type.
+
+    This function maps raw types to their corresponding Python types as
+    used in Snakemake. Snakemake primarily uses Python types for its parameters,
+    and this function ensures that the provided type is appropriately converted.
+
+    Args:
+        param: The raw type to be converted, which can be of any type.
+
+    Returns:
+        A string representing the corresponding Python type for Snakemake.
+
+    Raises:
+        TypeError: If the parameter's type cannot be mapped to a known Python type.
+    """
     typ = str(param)
     if typ.__contains__("str"):
         return f'"{typ.replace("str|", "")}"'
@@ -69,34 +100,75 @@ def to_snakemake_type(param: Raw) -> str:
     elif typ.__contains__("int"):
         return typ.replace("int|", "")
     else:
-        raise TypeError(f"Cannot render complex type ({typ}) to JSON")
-    
-# TODO: Refactor: better way to handle the types.
-# Question: I've seen Phil abstract methods like these into classless helper methods - why? It happens in go aswell
-# but I always thought that was because there's no classes
-def get_method_args(func: Lazy) -> ItemsView[str, inspect.Parameter]:
-    """Snakemake-renderable internal reference."""
+        raise TypeError(f"Cannot render complex type ({typ})")
+
+
+def get_method_args(func: Lazy) -> inspect.Signature:
+    """Retrieve the argument names and types of a lazy-evaluatable function.
+
+    Args:
+        func: A function that adheres to the `Lazy` protocol.
+
+    Returns:
+        An `ItemsView` object containing the argument names and their corresponding
+        `inspect.Parameter` objects.
+    """
     args = inspect.signature(func)
 
     return args
 
-def get_method_rel_path(func: Lazy) -> any:
-    """Snakemake-renderable internal reference."""
+
+def get_method_rel_path(func: Lazy) -> typing.Any:
+    """Get the relative path of the module containing the given function.
+
+    Args:
+        func (Lazy): The function for which the relative path is to be determined.
+
+    Returns:
+        any: The relative path of the module containing the function.
+
+    Note:
+        This function relies on the `inspect` and `os` modules to compute its relative path.
+    """
+    # TODO: error handling
     source_file = inspect.getsourcefile(func)
-    relative_path = os.path.relpath(source_file, start=os.getcwd())
-    module_name = os.path.splitext(relative_path)[0].replace(os.path.sep, '.')
+    if source_file:
+        relative_path = os.path.relpath(source_file, start=os.getcwd())
+        module_name = os.path.splitext(relative_path)[0].replace(os.path.sep, ".")
 
     return module_name
 
+
 @define
 class InputDefinition:
-    """Snakemake-renderable run block from a dewret workflow step."""
-    inputs: str
-    params: str
-    
-    # TODO: Refactor: Error Handling and better structure
+    """Represents input and parameter definitions block for a Snakemake-renderable workflow step.
+
+    Attributes:
+        inputs (List[str]): A list of input definitions.
+        params (List[str]): A list of parameter definitions.
+
+    Methods:
+        from_step(cls, step: Step) -> "InputDefinition": Constructs an InputDefinition
+            object from a Step object, extracting inputs and parameters and converting
+            them to Snakemake-compatible format.
+        render(self) -> dict[str, str]: Renders the input and parameter definitions
+            as a dictionary for use in Snakemake Input and Params blocks.
+    """
+
+    inputs: list[str]
+    params: list[str]
+
     @classmethod
     def from_step(cls, step: Step) -> "InputDefinition":
+        """Constructs an InputDefinition object from a Step.
+
+        Args:
+            step (Step): The Step object from which input and parameter block definitions are
+                extracted.
+
+        Returns:
+            InputDefinition: An InputDefinition object.
+        """
         params = []
         inputs = []
         for key, param in step.arguments.items():
@@ -104,150 +176,257 @@ class InputDefinition:
                 ref = ReferenceDefinition.from_reference(param).render()
                 input = f"{key}=rules.{ref.replace("-","_").replace("/out", ".output")}.output_file"
                 inputs.append(input)
-                param = f"{key}=input.{key},"
-                params.append(param)
-            else:
-                param = f"{key}={to_snakemake_type(param)},"
-                params.append(param)
+                params.append(input + ",")
+            elif isinstance(param, Raw):
+                customized = f"{key}={to_snakemake_type(param)},"
+                params.append(customized)
 
-        # # Check if the list is not empty
         if params:
-            params[len(params)-1] = params[len(params)-1].replace(",", "")
+            params[len(params) - 1] = params[len(params) - 1].replace(",", "")
 
-        return cls(
-            inputs=inputs,
-            params=params
-        )
-    
-    def render(self) -> dict[str, RawType]:
-        return {
-            "inputs": self.inputs,
-            "params": self.params
-        }
+        return cls(inputs=inputs, params=params)
+
+    def render(self) -> dict[str, list[str]]:
+        """Renders the input and parameter definitions as a dictionary.
+
+        Returns:
+            dict[str, list[RawType]]: A dictionary containing the input and parameter definitions,
+                for use in Snakemake Input and Params blocks.
+        """
+        return {"inputs": self.inputs, "params": self.params}
+
 
 @define
 class OutputDefinition:
-    """Snakemake-renderable run block from a dewret workflow step."""
-    output_file: str
-    # I need a way to find the output_file, for now I assume it'll be in the method signature
+    """Represents the output definition block for a Snakemake-renderable workflow step.
 
-    
-    # TODO: Refactor: Error Handling and better structure
+    Attributes:
+        output_file (str): The output file definition.
+
+    Methods:
+        from_step(cls, step: Step) -> "OutputDefinition": Constructs an OutputDefinition
+            object from a Step object, extracting and converting the output file definition
+            to Snakemake-compatible format.
+
+        render(self) -> list[str]: Renders the output definition as a list
+            suitable for use in Snakemake Output block.
+    """
+
+    output_file: str
+
     @classmethod
     def from_step(cls, step: Step) -> "OutputDefinition":
-        args = step.arguments["output_file"]
-        args = to_snakemake_type(args)
-        return cls(
-            output_file=args
-        )
-    
-    def render(self) -> tuple[str | RawType]:
-        return (
+        """Constructs an OutputDefinition object from a Step.
+
+        Args:
+            step (Step): The Step object from which the output file definition is extracted.
+
+        Returns:
+            OutputDefinition: An OutputDefinition object, for use in Snakemake Output block.
+        """
+        # TODO: Error handling
+        # TODO: Better way to handling input/output files
+        output_file = step.arguments["output_file"]
+        if isinstance(output_file, Raw):
+            args = to_snakemake_type(output_file)
+
+        return cls(output_file=args)
+
+    def render(self) -> list[str]:
+        """Renders the output definition as a list.
+
+        Returns:
+            list[str]: A list containing the output file definition, for use in a Snakemake Output block.
+        """
+        # The comma after the last element is mandatory for the structure of rule onces it's used in yaml.dump
+        return [
             f"output_file={self.output_file}",
-        )
+        ]
+
 
 @define
 class RunDefinition:
-    """Snakemake-renderable run block from a dewret workflow step."""
+    """Represents a Snakemake-renderable run block for a dewret workflow step.
+
+    Attributes:
+        method_name (str): The name of the method to be executed in the snakefile run block.
+        rel_import (str): The relative import path of the method.
+        args (List[str]): The arguments to be passed to the method.
+
+    Methods:
+        from_task(cls, task: Task) -> "RunDefinition": Constructs a RunDefinition
+            object from a Task object, extracting method information and arguments
+            from the task and converting them to Snakemake-compatible format.
+
+        render(self) -> list[str]: A list containing the import statement and the method
+            call statement, for use in Snakemake run block.
+    """
+
     method_name: str
     rel_import: str
     args: list[str]
 
-    
-    # TODO: Refactor: Error Handling and better structure
     @classmethod
     def from_task(cls, task: Task) -> "RunDefinition":
+        """Constructs a RunDefinition object from a Task.
+
+        Args:
+            task (Task): The Task object from which method information and arguments
+                are extracted.
+
+        Returns:
+            RunDefinition: A RunDefinition object containing the converted method
+                information and arguments.
+        """
         relative_path = get_method_rel_path(task.target)
         rel_import = f"{relative_path}"
 
-        # TODO: Make sure to use Paramdefinitions for getting the parameters
         args = get_method_args(task.target)
         signature = [
-            f"{param_name}=params.{param_name}" 
+            f"{param_name}=params.{param_name}"
             for param_name, param in args.parameters.items()
         ]
 
-        return cls(
-            method_name=task.name,
-            rel_import=rel_import,
-            args=signature
-        )
-    
-    def render(self) -> tuple[str | RawType]:
+        return cls(method_name=task.name, rel_import=rel_import, args=signature)
+
+    def render(self) -> list[str]:
+        """Renders the run block as a list of strings.
+
+        Returns:
+            list[str]: A list containing the import statement and the method
+                call statement, for use in Snakemake run block.
+        """
         signature = ", ".join(f"{arg}" for arg in self.args)
-        return (
+        # The comma after the last element is mandatory for the structure of rule onces it's used in yaml.dump
+        return [
             f"import {self.rel_import}\n",
             f"{self.rel_import}.{self.method_name}({signature})\n",
-        )
+        ]
+
 
 @define
 class StepDefinition:
-    """Snakemake-renderable rule from a dewret workflow step."""
+    """Represents a Snakemake-renderable step definition in a dewret workflow.
+
+    Attributes:
+        name (str): The name of the step.
+        run (str): The run block definition for the step.
+        params (List[str]): The parameter definitions for the step.
+        output (list[str]: The output definition for the step.
+        input (List[str]): The input definitions for the step.
+
+    Methods:
+        from_step(cls, step: Step) -> "StepDefinition": Constructs a StepDefinition
+            object from a Step object, extracting step information and components
+            from the step and converting them to Snakemake format.
+        render(self) -> dict[str, RawType]: Renders the step definition as a dictionary
+            suitable for use in Snakemake workflows.
+    """
 
     name: str
-    run: str
+    run: list[str]
     params: list[str]
-    output: tuple[RawType]
-    # in_: Mapping[str, ReferenceDefinition | Raw]
-    in_: list[str]
-
-    # TODO: Refactor: Use custom made RunDefinition to populate the run block of the snakefile
-    # TODO: Refactor: Use custom made InputDefinition to populate the input block of the snakefile
-    # TODO: Refactor: Use custom made ParamsDefinition to populate the params block of the snakefile
-    # TODO: Refactor: Use custom made RunDefinition to populate the run block of the snakefile
-    # TODO: Fix: TypeError: RunDefinition() takes no arguments
+    output: list[str]
+    input: list[str]
 
     @classmethod
-    def from_step(cls, step: Step) -> "StepDefinition":                
+    def from_step(cls, step: Step) -> "StepDefinition":
+        """Constructs a StepDefinition object from a Step.
+
+        Args:
+            step (Step): The Step object from which step information and components
+                are extracted.
+
+        Returns:
+            StepDefinition: A StepDefinition object containing the converted step
+                information and components.
+        """
         input_block = InputDefinition.from_step(step).render()
         run_block = RunDefinition.from_task(step.task).render()
         output_block = OutputDefinition.from_step(step).render()
         return cls(
-            name=step.name, 
-            run=run_block, 
-            params=input_block["params"], 
-            in_=input_block["inputs"],
-            output=output_block
+            name=step.name,
+            run=run_block,
+            params=input_block["params"],
+            input=input_block["inputs"],
+            output=output_block,
         )
 
-    # TODO: Fix: Params must be comma delimited or they get errors
-    # TODO: Fix: New line after output causes snakemake errors
-    # TODO: Fix: You can't call parameters from params block in output block
-    # TODO: Refactor: Create OutputDefinition to populate the output block of the snakefile
     def render(self) -> dict[str, RawType]:
+        """Renders the step definition as a dictionary.
+
+        Returns:
+            dict[str, RawType]: A dictionary containing the components of the step
+                definition, for use in Snakemake workflows.
+        """
         return {
             "run": self.run,
-            "input": self.in_,
+            "input": self.input,
             "params": self.params,
             "output": self.output,
         }
 
 
-# TODO: Fix: Find out why the order of the rules is scrambled
-# TODO: Fix: Add a rule all: with input definition the last outputed file
+# TODO: Find out why the order of the rules is scrambled
+# FIXME: Add a rule all: with input definition the last outputed file
+
+
 @define
 class WorkflowDefinition:
+    """Represents a Snakemake-renderable workflow definition from a dewret workflow.
+
+    Attributes:
+        steps (List[StepDefinition]): A list of StepDefinition objects representing the steps
+            in the workflow.
+
+    Methods:
+        from_workflow(cls, workflow: Workflow) -> "WorkflowDefinition": Constructs a WorkflowDefinition
+            object from a Workflow object, converting its steps to StepDefinition objects.
+
+        render(self) -> dict[str, RawType]: Renders the workflow definition as a dictionary
+            containing Snakemake rules, for use in Snakemake workflows.
+    """
+
     steps: list[StepDefinition]
 
-    # Question: Is it more abstract if I define the different blocks in the Workflow definition or the steps definition?
+    # TODO: Add a "rule all" definition
+    # In order for snakemake to execute multiple connected rules
+    # it needs a rule to define which is the target rule that connects all the multiple rules
     @classmethod
     def from_workflow(cls, workflow: Workflow) -> "WorkflowDefinition":
-        return cls(
-            steps=[
-                StepDefinition.from_step(step)
-                for step in workflow.steps
-            ]
-        )
+        """Creates a WorkflowDefinition object from a Workflow object.
+
+        Args:
+            workflow (Workflow): The workflow to be rendered.
+
+        Returns:
+            str: WorkflowDefinition with definited steps.
+        """
+        return cls(steps=[StepDefinition.from_step(step) for step in workflow.steps])
 
     def render(self) -> dict[str, RawType]:
+        """Render the WorkflowDefinition.
+
+        Returns:
+            dict[str, RawType]: A dictionary containing the components of the Workflow
+                definition, for use in Snakemake workflows.
+        """
         return {
             f"rule {step.name.replace("-", "_")}": step.render() for step in self.steps
         }
 
 
-# Takes a dewret workflow object and creates a workflow based on the workflow language.
 def render(workflow: Workflow) -> str:
-    """Render to a SMK string."""
+    """Render the workflow as a Snakemake (SMK) string.
+
+    This function converts a Workflow object into a Snakemake-compatible yamp.
+
+    Args:
+        workflow (Workflow): The workflow to be rendered.
+
+    Returns:
+        str: A Snakemake-compatible yaml representation of the workflow.
+    """
     trans_table = str.maketrans(
         {
             "-": "   ",
@@ -256,11 +435,6 @@ def render(workflow: Workflow) -> str:
             "]": "",
         }
     )
-    print(
-        yaml.dump(
-            WorkflowDefinition.from_workflow(workflow).render(), indent=4
-        ).translate(trans_table).replace("!!python/tuple", "")
-    )
     return yaml.dump(
         WorkflowDefinition.from_workflow(workflow).render(), indent=4
-    ).translate(trans_table).replace("!!python/tuple", "")
+    ).translate(trans_table)
---
+++ example/snakemake_renderer/basic_example/snakemake_tasks.py
@@ -0,0 +1,71 @@
+"""Basic snakemake workflow.
+
+Useful as an example of a workflow with multiple tasks.
+
+```sh
+$ python -m dewret workflow_complex.py --pretty run
+```
+"""
+
+from dewret.tasks import task, construct
+from renderer import render
+
+STARTING_NUMBER: int = 23
+
+
+@task()
+def download_data(data: str, output_file: str) -> str:
+    """Example task to be converted to snakemake workflow."""
+    with open(output_file, "w") as f:
+        f.write(data)
+
+    return output_file
+
+
+@task()
+def process_data(data_file: str, multiple_arg: str, output_file: str) -> str:
+    """Example task to be converted to snakemake workflow."""
+    new_data = "test"
+
+    with open(data_file, "r") as f:
+        data = f.read()
+        new_data = data + new_data + multiple_arg
+
+    with open(output_file, "w") as f:
+        f.write(new_data)
+
+    return output_file
+
+
+@task()
+def generate_report(processed_data: str, multiple_arg: str, output_file: str) -> str:
+    """Example task to be converted to snakemake workflow."""
+    with open(processed_data, "r") as datafile:
+        data = datafile.read()
+
+    with open(output_file, "w") as f:
+        f.write(data + multiple_arg)
+
+    return output_file
+
+
+if __name__ == "__main__":
+    data = download_data(
+        data="4, 5, 7, 8, 0", output_file="generated_smk/data/test_data.txt"
+    )
+    processed_data = process_data(
+        data_file=data,
+        multiple_arg="test1",
+        output_file="generated_smk/data/processed_data.txt",
+    )
+    result = generate_report(
+        processed_data=processed_data,
+        multiple_arg="test2",
+        output_file="results/report.txt",
+    )
+
+    workflow = construct(result)
+    smk_output = render(workflow)
+
+    with open("Snakefile", "w") as file:
+        file.write(smk_output)
