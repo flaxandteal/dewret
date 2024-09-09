7ae5a7a335ab5c5da826f2cbbb1b278526f17f85
Phil Weir <phil.weir@flaxandteal.co.uk>
2024-09-09 21:59:53 +0100

    fix: double-check importing correctly errors if render neither behaves as a package nor isolated module

---
+++ tests/_lib/unfrender.py
@@ -0,0 +1,29 @@
+"""Testing example renderer.
+
+Correctly fails to import to show a broken module being handled.
+"""
+
+from typing import Unpack, TypedDict
+from dewret.workflow import Workflow
+
+# This lacking a relative import, while extra itself
+# uses one is what breaks the module. It cannot be both
+# a package and not-a-package. This is importable by
+# adding a . before extra. If instead, you try to avoid
+# relative imports altogether, and change .other -> other
+# in extra, it will also break, as the directory of this
+# file is not in the PATH.
+from extra import JUMP # type: ignore
+
+class UnfrenderRendererConfiguration(TypedDict):
+    allow_complex_types: bool
+
+def default_config() -> UnfrenderRendererConfiguration:
+    return {
+        "allow_complex_types": True
+    }
+
+def render_raw(
+    workflow: Workflow, **kwargs: Unpack[UnfrenderRendererConfiguration]
+) -> dict[str, str]:
+    return {"JUMP": str(JUMP)}
