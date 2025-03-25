b02c85187ee3c436a7f4fc91300efc0a85843d81
eduardo.serna <eduardo.serna@flaxandteal.co.uk>
2025-03-25 18:16:53 +0100

    test: add test for https://github.com/flaxandteal/dewret/issues/67
    fix: remove # type ignore, which has to be committed with --no-verify to stop CI from complaining

---
+++ tests/test_cli.py
@@ -0,0 +1,45 @@
+"""Test CLI render function."""
+
+import yaml
+from pathlib import Path
+import pytest
+from dewret.__main__ import render # without # type ignore, local mypy complains, but with it, the CI mypy complains, so this has to be committed with --no-verify to stop CI from complaining
+
+
+def test_cli_call() -> None:
+    """Test CLI render function."""
+    # Setup
+    test_dir = Path(__file__).parent
+    repo_root = test_dir.parent
+
+    example_file = repo_root / "example" / "workflow_qs.py"
+    output_file = (test_dir / "tmp_file").resolve()
+
+    try:
+        # not sure why it is raising an exception
+        with pytest.raises(SystemExit) as exc_info:
+            render(
+                [
+                    "--pretty",
+                    "--backend",
+                    "DASK",
+                    "--output",
+                    str(output_file),
+                    example_file.as_posix(),
+                    "increment",
+                    "num:3",
+                ]
+            )
+
+        # Check that it was a successful exit (code 0)
+        assert exc_info.value.code == 0
+
+        # Now check the output file
+        with open(output_file, "r") as file:
+            res = yaml.safe_load(file)
+
+        assert list(res.keys()) == ["class", "cwlVersion", "inputs", "outputs", "steps"]
+    finally:
+        # Delete the file in the finally block to ensure it's always cleaned up
+        if output_file.exists():
+            output_file.unlink()
